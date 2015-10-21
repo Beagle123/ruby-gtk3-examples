ee1106e4893177a6590836b5073393b25ebdba98
Andrew Steele <andrew@andrewsteele.me.uk>
2015-10-21 19:49:30 +0100

    Add LinkButton demonstration

---
+++ linkbutton.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'gtk3'
+
+window = Gtk::Window.new
+window.set_title('LinkButton')
+window.signal_connect('delete-event') {
+    Gtk::main_quit
+}
+
+linkbutton = Gtk::LinkButton.new('Go to the Programmica website')
+linkbutton.set_uri('http://programmica.com')
+window.add(linkbutton)
+
+window.show_all
+
+Gtk.main
