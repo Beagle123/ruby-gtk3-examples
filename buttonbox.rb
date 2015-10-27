b14f58d0790ef2feaa531074ff57db57ed78e095
Andrew Steele <andrew@andrewsteele.me.uk>
2015-10-27 17:02:42 +0000

    Added example of ButtonBox usage

---
+++ buttonbox.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'gtk3'
+
+window = Gtk::Window.new
+window.set_title('ButtonBox')
+window.signal_connect("delete-event") {
+    Gtk::main_quit
+}
+
+buttonbox = Gtk::ButtonBox.new :horizontal
+window.add(buttonbox)
+
+button = Gtk::Button.new :label => "Single-line button"
+buttonbox.add(button)
+button = Gtk::Button.new :label => "Multi-line\nbutton"
+buttonbox.add(button)
+button = Gtk::Button.new :label => "Extra long multi-line\nbutton"
+buttonbox.add(button)
+
+window.show_all
+
+Gtk.main
